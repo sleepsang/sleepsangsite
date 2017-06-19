7129298b2570cc58c57a23bece9742e5bc87d728
james <jfrog8000@gmail.com>
2017-06-19 17:44:47 +0100

    css/ss_mainpage1.css

--- sketchy.js
+++ sketchy.js
@@ -1,14 +1,26 @@
-//"use strict";
+//var img;  // Declare variable 'img'.
 
 function setup() {
-    createCanvas(640, 480);
+  createCanvas(960, 540);
 }
 
 function draw() {
-  if (mouseIsPressed) {
+    if (mouseIsPressed) {
     fill(0);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80); 
+  ellipse(mouseX, mouseY, 80, 80);
 }
+
+/* 
+shifting gaussian blur to imitate the effect of a heat haze mirage from surfaces (grounds on hot days etc) 
+
+    - create a function which blurs only a vertical slice of the image (filter blur)
+    - change vertical slice to zig zag
+    - curve the line between points on the zig zag
+    - animate the lines of the zig zag so it wobbles slightly
+    - nest within horizontal movement through the image with wraparound
+    
+    * deviceMoved, deviceShaken? accelerationX, pAccelerationX?
+*/
