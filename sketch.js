95f2a00f5881a83b82b40ea9ab08a1b5464c4063
james <jfrog8000@gmail.com>
2018-04-10 13:34:43 +0100

    half way through attempting to use souncloud api on click play

--- sketch.js
+++ sketch.js
@@ -1,42 +1,64 @@
 var canvas;
+var widgetIframe = document.getElementById('sc-widget');
 
 function resizeWindow() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function mouseClicked() {
+  console.log("hi");
+      widget       = SC.Widget(widgetIframe);
+      widget.bind(SC.Widget.Events.READY, function() {
+           widget.bind(SC.Widget.Events.PLAY, function() {
+             // get information about currently playing sound
+             widget.getCurrentSound(function(currentSound) {
+               console.log('sound ' + currentSound.get('') + 'began to play');
+             });
+           });
+         };
+         *
+}
+
+
+
 function setup() {
 
- for (var i = 0; i < 4; i++) {
-   wanderers[i] = new Wanderer();
- }
+   for (var i = 0; i < 4; i++) {
+     wanderers[i] = new Wanderer();
+   }
 
- for (var i = 0; i < 40; i++) {
-   followers[i] = new Follower();
- }
+   for (var i = 0; i < 40; i++) {
+     followers[i] = new Follower();
+   }
 
 }
 
 function draw() {
-   canvas = createCanvas(windowWidth, windowHeight);
-   canvas.position(0,0);
-   canvas.style('z-index','-1');
-  for (var i = 0; i < wanderers.length; i++) {
-    wanderers[i].wanshow();
-    wanderers[i].wanmove();
+
+       canvas = createCanvas(windowWidth, windowHeight);
+       canvas.position(0,0);
+       canvas.style('z-index','-1');
+      for (var i = 0; i < wanderers.length; i++) {
+        wanderers[i].wanshow();
+        wanderers[i].wanmove();
+    }
+
+    for (var i = 0; i < followers.length; i++) {
+        followers[i].folshow();
+        followers[i].folmove();
+    }
+
+      if ( mouseX > width/16 ) {
+        strokeWeight(0);
+        fill(Math.floor(random(195,255)),Math.floor(random(200,255)),Math.floor(random(235,240)));
+        ellipse(Math.floor(random(0, width)),Math.floor(random(0, height)),10,10);
+
+      }
+
 }
 
-for (var i = 0; i < followers.length; i++) {
-    followers[i].folshow();
-    followers[i].folmove();
-}
 
-}
 
-/*
-  if ( mouseX > width/2 ) {
-    ellipse(Math.floor(random(0, width)),Math.floor(random(0, height)),10,11);
-  }
-  */ //random dots when mouse is on right side of canvas
 
 /*
 - explore sound libraries or ways of interfacing between p5 and supcol/pd. each random gen dot to trigger note etc
